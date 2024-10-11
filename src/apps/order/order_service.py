5ec448be48f57ca67b4ce0a9ed50a525c5cb6dce
Maxson Almeida <maxsonferovante@gmail.com>
2024-10-10 22:03:05 -0300

    Refactorizar config.py para adicionar configurações do Redis e importar o módulo de tarefas

--- src/apps/order/order_service.py
+++ src/apps/order/order_service.py
@@ -1,5 +1,5 @@
 from src.providers.potiguar_lookup.potiguar_lookup_service import PotiguarLookupService
-
+from src.apps.tasks.tasks_service import TasksService
 from .order_model import Order, OrderCreateDTO, transform_OrderCreateDTO_to_order, OrderFindDTO, OrderStatus
 from .order_entity import Order as OrderEntity, transform_Document_to_Order
 from .exceptions.order_not_found_exception import OrderNotFoundException
@@ -11,8 +11,9 @@ from fastapi import HTTPException
 
 @Injectable
 class OrderService:
-    def __init__(self, potiguar_lookup_service: PotiguarLookupService):
+    def __init__(self, potiguar_lookup_service: PotiguarLookupService, tasks_service: TasksService):
         self.potiguar_lookup_service = potiguar_lookup_service
+        self.tasks_service = tasks_service
 
 
     @db_request_handler
@@ -20,17 +21,25 @@ class OrderService:
         
         orderModel = transform_OrderCreateDTO_to_order(order)
         
-        vehicle_data = await self.potiguar_lookup_service.get_vehicle_data(order.dict())
-        vehicle_debts = await self.potiguar_lookup_service.obtain_vehicle_debts(vehicle_data)
-        orderModel.result = {
-            "vehicle_data": vehicle_data,
-            "vehicle_debts": vehicle_debts
-        }
-        orderModel.status = OrderStatus.COMPLETED
+        # vehicle_data = await self.potiguar_lookup_service.get_vehicle_data(order.dict())
+        # vehicle_debts = await self.potiguar_lookup_service.obtain_vehicle_debts(vehicle_data)
+        # orderModel.result = {
+        #     "vehicle_data": vehicle_data,
+        #     "vehicle_debts": vehicle_debts
+        # }
+        # orderModel.status = OrderStatus.COMPLETED
         
         new_order = OrderEntity(**orderModel.dict())
         
         await new_order.save()
+        
+        message = self.tasks_service.run_get_vehicle_data(
+            license_plate=orderModel.license_plate,
+            renavam=orderModel.renavam,
+            identifier=orderModel.identifier   
+        )
+        
+        orderModel.result = message
         
         return orderModel
 
--- src/apps/tasks/tasks_service.py
+++ src/apps/tasks/tasks_service.py
@@ -1,15 +1,57 @@
+import asyncio
+from celery import Celery, chain
+from typing import Dict
 from nest.core import Injectable
 from src.providers.potiguar_lookup.potiguar_lookup_service import PotiguarLookupService
+from src.providers.recaptcha.recaptcha_service import RecaptchaService
+from src.apps.order.order_entity import Order as OrderEntity, OrderStatus
+from src.config import config_redis
+
+celery_manager = Celery(
+    'tasks',
+    broker=config_redis['url'],
+    backend=config_redis['url']
+)
+
+potiguar_lookup_service = PotiguarLookupService(RecaptchaService())
 
 @Injectable
 class TasksService:
 
-    def __init__(self, potiguar_lookup_service: PotiguarLookupService):
-        self.potiguar_lookup_service = potiguar_lookup_service
 
+    def run_get_vehicle_data(self, license_plate: str, renavam: str, identifier: str):       
+        try:
+            print("Starting task for license_plate: {} and renavam: {} -- {}".format(license_plate, renavam, identifier))
 
-    async def add_tasks(self):
-        pass
+            get_vehicle_data.delay(license_plate, renavam, identifier)
+            
+            return {
+                "message": "Task completed for license_plate: {} and renavam: {}".format(license_plate, renavam)
+            }
+        except Exception as e:
+            print("Error: {}".format(e))
     
-    async def get_tasks(self):
-        pass
+        
+@celery_manager.task(name='get_vehicle_data',retry_backoff=True,autoretry_for=(Exception,))
+def get_vehicle_data(license_plate: str, renavam: str, identifier: str):
+    vehicle_data = asyncio.run(potiguar_lookup_service.obtain_vehicle_data(
+        license_plate=license_plate,
+        renavam=renavam
+    ))
+    vehicle_debts = asyncio.run(potiguar_lookup_service.obtain_vehicle_debts(vehicle_data=vehicle_data))
+    
+    save_vehicle_data.delay(vehicle_data, vehicle_debts, identifier)
+ 
+@celery_manager.task(name='save_vehicle_data',bind=True, retry_backoff=True,autoretry_for=())   
+def save_vehicle_data(vehicle_data: Dict, vehicle_debts: Dict, identifier: str):
+    order_find = asyncio.run(OrderEntity.find_one(OrderEntity.identifier == identifier))
+    
+    order_find.result = {
+        "vehicle_data": 'vehicle_data',
+        "vehicle_debts": 'vehicle_debts',
+        }
+    order_find.status = OrderStatus.COMPLETED
+    asyncio.run(order_find.save())
+        
+        
+       
