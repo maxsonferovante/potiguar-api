8541faaaf3958cf493db01234a39948d954ba50b
Maxson Almeida <maxsonferovante@gmail.com>
2024-10-11 22:53:51 -0300

    Add RateLimitMiddleware to handle rate limiting for API requests

--- src/app_module.py
+++ src/app_module.py
@@ -5,6 +5,7 @@ from .app_service import AppService
 from src.apps.order.order_module import OrderModule
 from src.apps.tasks.tasks_module import TasksModule
 
+from src.middlewares.rate_limit_middleware import RateLimitMiddleware
 
 @Module(
     imports=[OrderModule, TasksModule],
@@ -17,14 +18,14 @@ class AppModule:
 
 app = PyNestFactory.create(
     AppModule,
-    description="This is my Async PyNest app.",
-    title="PyNest Application",
+    description="Potiguar API is a service designed to facilitate the consultation of vehicle infractions, subsidies, and fines in Rio Grande do Norte. It provides a robust and efficient way to access and manage this information through a well-structured API.",
+    title="Potiguar API",
     version="1.0.0",
-    debug=True,
     docs_url="/api/docs",
 )
 http_server = app.get_server()
-
+#  - RateLimitMiddleware: Limit the number of requests per client IP.
+http_server.add_middleware(RateLimitMiddleware, max_requests=60, window_seconds=60)
 
 @http_server.on_event("startup")
 async def startup():
