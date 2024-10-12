93914f7702a027be3de704abb7acc8c9e7d2bbc8
Maxson Almeida <maxsonferovante@gmail.com>
2024-10-11 22:41:58 -0300

    - Adicionar nova exceção ConflictProcessOrderException em handlers_order_expections.py - Adicionar tratamento para a exceção ConflictProcessOrderException em handlers_order_expections.py - Adicionar novo arquivo conflict_order_exception.py - Atualizar order_controller.py para tratar exceções e imprimir mensagens de erro

--- src/apps/order/exceptions/handlers_order_expections.py
+++ src/apps/order/exceptions/handlers_order_expections.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException, status as HTTPStatus
 from .identifier_not_valid import IdentifierNotValid
 from .order_not_found_exception import OrderNotFoundException
-
+from .conflict_order_exception import ConflictProcessOrderException
 
 def handlers_order_expections(exception: Exception):
     if isinstance(exception, OrderNotFoundException):
@@ -22,6 +22,17 @@ def handlers_order_expections(exception: Exception):
                 "type": "IdentifierNotValid"
             })
     
+    if isinstance(exception, ConflictProcessOrderException):
+        raise HTTPException(
+            status_code=HTTPStatus.HTTP_409_CONFLICT, 
+            detail= {
+                "message": str(exception),
+                "license_plate": exception.license_plate,
+                "renavam": exception.renavam,
+                "identifier": exception.identifier,
+                "task_id": exception.task_id,
+                "type": "ConflictProcessOrderException"
+            })
     raise HTTPException(status_code=500, 
                         detail= {
                             "message": str(exception),
