0a6e890f79af0af44650335a9f7d0d856663ddfd
Maxson Almeida <maxsonferovante@gmail.com>
2024-10-13 16:40:38 -0300

    Refatorar exceção RecaptchaExpectedException e remover classe não utilizada

--- src/providers/recaptcha/recaptcha_service.py
+++ src/providers/recaptcha/recaptcha_service.py
@@ -20,28 +20,23 @@ class RecaptchaService:
     
     async def get_recaptcha(self) -> str:
         async with httpx.AsyncClient() as client:
-            try:
-                payload = {
+            payload = {
                     "clientKey": self.client_key,
                     "task": self.task
                 }
-                print ("Payload", payload)
-                response = await client.post(f"{self.url_captcha}/createTask", json=payload, timeout=30)
-                task_id = response.json().get("taskId")
-                if not task_id:
-                    raise FaildCreateTaskException("Failed to create task: {}".format(response.json()))
-                while True:
-                    await asyncio.sleep(1)
-                    response = await client.post(f"{self.url_captcha}/getTaskResult", json={"clientKey": self.client_key, "taskId": task_id}, timeout=30)
-                    status = response.json().get("status")
-                    if status == "ready":
-                        return response.json().get("solution", {}).get('gRecaptchaResponse')
-                    if status == "failed" or response.json().get("errorId"):
-                        raise FaildSolutionException("Failed to solve captcha: {}".format(response.json()))
-            except Exception as e:
-                if isinstance(e, Exception):
-                    raise e
-                raise e
-            
+    
+            response = await client.post(f"{self.url_captcha}/createTask", json=payload, timeout=30)
+            task_id = response.json().get("taskId")
+            if not task_id:
+                raise FaildCreateTaskException("Failed to create task: {}".format(response.json()))
+            while True:
+                await asyncio.sleep(1)
+                response = await client.post(f"{self.url_captcha}/getTaskResult", json={"clientKey": self.client_key, "taskId": task_id}, timeout=30)
+                status = response.json().get("status")
+                if status == "ready":
+                    return response.json().get("solution", {}).get('gRecaptchaResponse')
+                if status == "failed" or response.json().get("errorId"):
+                    raise FaildSolutionException("Failed to solve captcha: {}".format(response.json()))            
+                
             
         
